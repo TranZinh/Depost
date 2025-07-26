3d19879e636705b018150984afd488f0cb072e4f
So Vinh <gift4you4143@gmail.com>
2025-07-26 15:49:25 +0700

    Post PJ

---
+++ SDL2_Roguelike_Survivor-master (1)/SDL2_Roguelike_Survivor-master/Headers/Utils/Map.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include <string>
+#include <vector>
+#include <SDL.h>
+
+class Map {
+public:
+    Map(const std::string& tilesetId, int tileSize);
+    ~Map();
+
+    // SỬA: Thay thế LoadMap bằng GenerateMap
+    void GenerateMap(int widthInTiles, int heightInTiles);
+
+    void Render(SDL_Renderer* renderer);
+
+    int GetTileIdAt(int x, int y) const; // <--- THÊM HÀM NÀY
+    int GetWorldWidth() const;
+    int GetWorldHeight() const;
+
+private:
+    std::string m_tilesetId;
+    SDL_Texture* m_pTilesetTexture;
+    int m_tileSize;
+    int m_mapWidthInTiles;
+    int m_mapHeightInTiles;
+    int m_tilesetWidthInTiles;
+    std::vector<std::vector<int>> m_mapData;
+};
